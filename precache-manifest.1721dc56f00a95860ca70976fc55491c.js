f153f60e6af3d98fa22e2e97d937310729e904a3
ijcon40 <ijcon40@gmail.com>
2020-09-26 22:12:51 -0400

    Updates

---
+++ precache-manifest.1721dc56f00a95860ca70976fc55491c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cbc169fc6c91e8e4e55aefbe919f34b",
+    "url": "/coverpage/index.html"
+  },
+  {
+    "revision": "eb5d6b03161585b03e03",
+    "url": "/coverpage/static/js/2.e750afab.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/coverpage/static/js/2.e750afab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345653f7d7ff5824a4b7",
+    "url": "/coverpage/static/js/main.f5aaad63.chunk.js"
+  },
+  {
+    "revision": "d5505443f76230efc9fd",
+    "url": "/coverpage/static/js/runtime-main.72e25c7b.js"
+  }
+]);
